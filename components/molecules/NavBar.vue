7dea579d167c2c7c279a31de0ed4f3911b1998bd
Samuel Bruton <samuel.bruton@ca.vu>
2024-11-20 04:58:37 +0000

    added navbar

---
+++ components/molecules/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="bg-gray-800 text-white p-4">
+    <ul class="flex space-x-4">
+      <li v-for="link in links" :key="link.name">
+        <NuxtLink :to="link.path" class="hover:text-blue-300">
+          {{ link.name }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { name: 'Home', path: '/' },
+        { name: 'Articles', path: '/articles' },
+        // Add other links as needed
+      ]
+    };
+  }
+}
+</script>
+
+<style scoped>
+nav {
+  @apply bg-gray-900;
+}
+a {
+  @apply text-white hover:text-blue-400;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NavBar />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+import NavBar from '@/components/molecules/NavBar.vue'
+</script>
