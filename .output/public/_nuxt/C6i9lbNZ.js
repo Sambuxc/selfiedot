20b4c0eb112eee9e0bb9a6284ee8ee90d44e07c5
SAM BRUTON <107853900+Sambuxc@users.noreply.github.com>
2024-11-26 08:47:33 +0000

    upload output dir for gh pages

---
+++ .output/public/_nuxt/C6i9lbNZ.js
@@ -0,0 +1,1 @@
+import{R as s}from"./BBGZjNxc.js";import{u}from"./CbzFMUn6.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
